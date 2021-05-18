edfcd353bbfcaf3746ee95aef5822456b3234240
Tobi <tobid7@outlook.de>
2021-05-18 19:02:23 +0200

    Update CI.sh

--- CI.sh
+++ CI.sh
@@ -1,1 +1,11 @@
 echo Installing
+
+          curl -o pacman.deb https://github.com/devkitPro/pacman/releases/download/v1.0.2/devkitpro-pacman.amd64.deb
+          sudo apt-get install gdebi-core
+          sudo chmod +x pacman.deb
+          sudo gdebi pacman.deb
+          git clone https://github.com/Tobi-D7/lpp-3ds-updated.git
+          cd lpp-3ds-updated
+          cp -d dka/devkitARM-r45 /opt/devkitpro/dka-r45
+          export DEVKITPRO=/opt/devkitpro
+          export DEVKITARM=/opt/devkitpro/dka-r45
