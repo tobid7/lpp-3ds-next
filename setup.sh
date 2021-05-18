573b3b3ecdec26b51403c082ec79c31543cd9eb3
Tobi <tobid7@outlook.de>
2021-05-18 19:12:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ echo Installing
           sudo gdebi pacman.deb
           git clone https://github.com/Tobi-D7/lpp-3ds-updated.git
           cd lpp-3ds-updated
-          cp -d dka/devkitARM-r45 /opt/devkitpro/dka-r45
+          sudo mkdir /opt/devkitpro
+          cp -d dka/devkitARM-r45/ /opt/devkitpro/dka-r45
           export DEVKITPRO=/opt/devkitpro
           export DEVKITARM=/opt/devkitpro/dka-r45
           git clone https://github.com/smealum/aemstro.git
